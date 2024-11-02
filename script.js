ee73b103941d067133615e6788f8893ff8be0762
Dakshayani-14 <99874182+Dakshayani-14@users.noreply.github.com>
2024-11-02 18:50:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function addToCart(productName, price) 
+{
+    alert('${productName} has been added to your cart for ₹$(price)}');
+    // Here, you can implement more complex cart functionality
+}
